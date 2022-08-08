e1b73f110225e344c3e95abf7938f7ed30ef05ec
dollar <tmg1999@yahoo.com>
2022-08-08 04:29:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_Template
+{
+    class Program
+    {
+        static void Main()
+        {
+            ///Main method makes a call to this problem's dedicated method, then prints the value and exits.
+            
+            //here are this problem's variables
+            int returnItem;
+            int[] argumentItem = new int[] { 0, 1, 0, 3, 2, 3 };
+
+            //method call
+            returnItem = LongestIncreasingSubsequence(argumentItem);
+
+            //print value
+            Console.WriteLine("RETURN ITEM: " + returnItem);
+        }
+
+        static int LongestIncreasingSubsequence(int[] nums)
+        {
+            ///LeetCode #300
+            
+            //create List and add first item of array, then iterate through array
+            List<int> subList = new() { nums[0] };
+            for (int i = 1; i < nums.Length; i++)
+            {
+                //if greater than item at last index of List, add to List
+                if (nums[i] > subList[^1])
+                {
+                    subList.Add(nums[i]);
+                }
+                //else if smaller than item at FIRST index of List, replace first item
+                else if (nums[i] <= subList[0])
+                {
+                    subList[0] = nums[i];
+                }
+                //else somewhere in between first and last items, so insert right after closest smaller item
+                else
+                {
+                    //iterate through each item in subList
+                    for (int j = subList.Count - 1; j > 0; j--)
+                    {
+                        //if smaller than this index but larger than previous, replace this and break
+                        if (nums[i] > subList[j - 1])
+                        {
+                            subList[j] = nums[i];
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            return subList.Count;
+        }
+    }
+}
